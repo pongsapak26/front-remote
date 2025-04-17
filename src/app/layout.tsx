7c6342223f7e383fe1ad0388686cf2f1b8966d89
Pongsapak Premsumran <90520922+pongsapak26@users.noreply.github.com>
2025-04-17 20:28:52 +0700

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/context/UserContext";
+import Vanta from "@/components/Vanta";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,13 +31,13 @@ export default function RootLayout({
         id="BGALL"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
-          <div className="flex flex-col w-full h-screen bg-gray-900 text-white">
+        <Vanta>
+          <div className="flex flex-col w-full h-screen text-white">
             <UserProvider>
               <div className="container mx-auto mt-10">{children}</div>
             </UserProvider>
           </div>
-        </div>
+        </Vanta>
       </body>
     </html>
   );
---
+++ src/components/Vanta.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import Script from "next/script";
+import React, { useEffect, useRef } from "react";
+
+const Vanta = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const vantaRef = useRef<HTMLDivElement>(null);
+  let vantaEffect: any = null;
+
+  useEffect(() => {
+    // ทำงานเมื่อ window พร้อม
+    if (
+      typeof window !== "undefined" &&
+      (window as any).VANTA &&
+      vantaRef.current
+    ) {
+      vantaEffect = (window as any).VANTA.NET({
+        el: vantaRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xca5252,
+        backgroundColor: 0x0,
+        points: 20.00,
+        maxDistance: 32.00,
+        spacing: 20.00
+      });
+    }
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, []);
+  return (
+    <>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
+        strategy="beforeInteractive"
+      />
+
+      <div
+        ref={vantaRef}
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Vanta;
